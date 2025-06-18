cf7e37f32c6e9d8d0d3697d17b14b742723516a2
9kaluga <9kaluga@gmail.com>
2025-06-18 23:33:11 +0300

    добавил список покемонов и анимацию выбора

--- src/pages/PokedexPage/PokedexPage.tsx
+++ src/pages/PokedexPage/PokedexPage.tsx
@@ -3,27 +3,47 @@ import styles from './pokedexPage.module.scss'
 import { useRequestPokemonQueries } from '../../utils/api/hooks/pokemon';
 
 export const PokemdexPage: React.FC = () => {
-  const [offset, setOffset] = useState(3);
+  const [offset, setOffset] = useState(5);
+  const [selectedPokemonId, setSelectedPokemonId] = useState(1);
   const results = useRequestPokemonQueries({ offset });
 
   const isLoading = results.some((result) => result.isLoading)
   if (isLoading) return null;
 
   const pokemons = results.map((result: any) => result.data.data);
-  console.log(pokemons); 
+  console.log('pokemons: ', pokemons); 
+  const selectedPokemon = pokemons.find((pokemon) => selectedPokemonId === pokemon.id)
+  console.log('selectedPokemon: ', selectedPokemon);
   
   return (
-    <div className={styles.page_container}>
-        <div className={styles.content_container}>
-          <div className={styles.card_container}>card </div>
-          <ul className={styles.list_container}>
+    <div className={styles.page}>
+        <div className={styles.content}>
+          <div className={styles.card}>
+            <div>title</div>
+            <div>image</div>
+            <div>statistic</div>
+
+          </div>
+          <ul className={styles.list}>
             list
             {pokemons.map((pokemon) => (
-              <li 
-                className={styles.pokemon_item}>
-                <img className={styles.pokemon_item_img} 
-                src={pokemon.sprites.front_default ?? ''} alt="pokemon img" /> 
-                {pokemon.name}
+                <li onClick={() => setSelectedPokemonId(pokemon.id)}
+                  tabIndex={0}
+                  role='option'
+                  aria-selected={setSelectedPokemonId === pokemon.id}
+                  onKeyPress={(event) => {
+                    if (event.key === 'Enter') setSelectedPokemonId(pokemon.id)
+                  }}
+                  key={pokemon.id}
+                  className={styles.pokemon_item}>
+                  <img className={styles.pokemon_item_img} 
+                  src={pokemon.sprites.front_default ?? ''} 
+                  alt="pokemon img" 
+                  /> 
+                  <div 
+                    className={styles.pokemon_item_text}>
+                    {pokemon.name}
+                  </div>
                 </li> 
               ))}
           </ul>
