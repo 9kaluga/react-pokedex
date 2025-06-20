79350ea88159bc90a21d0d9dba9971c8a83d538f
9kaluga <9kaluga@gmail.com>
2025-06-20 21:29:44 +0300

    add stats, abil, types and styles

--- src/pages/PokedexPage/PokedexPage.tsx
+++ src/pages/PokedexPage/PokedexPage.tsx
@@ -3,6 +3,15 @@ import styles from './pokedexPage.module.scss'
 import { useRequestPokemonQueries } from '../../utils/api/hooks/pokemon';
 // import { useRequestStatQuery } from '../../utils/api/hooks';
 
+type StatList = {
+  base_stat: number;
+  stat: {name: string};
+}
+
+type AbilityList = {
+  ability: {name: string};
+}
+
 export const PokemdexPage: React.FC = () => {
   const [offset, setOffset] = useState(6);
   const [selectedPokemonId, setSelectedPokemonId] = useState(1);
@@ -28,32 +37,53 @@ export const PokemdexPage: React.FC = () => {
               <div className={styles.card_title_name}>{selectedPokemon.name}</div>
               <div className={styles.card_title_id}>#00{selectedPokemon.id}</div>
             </div>
+            <div className={styles.card_types}>
+              {selectedPokemon.types.map((type) => (
+                <div className={styles.card_type}>
+                  {type.type.name}
+                </div>
+              ))}
+            </div>
             <div>
               <img src={selectedPokemon.sprites.front_default ?? ''} 
                   alt="pokemon img" />
             </div>
-            <div>
-              <ul>{selectedPokemon.stats.map((stat: {stat: {name: string}}) => (
-                <li>
-                {stat.stat.name}
-                </li>
-                ))}
-              </ul>
+            <div className={styles.card_info}>
+              <div className={styles.card_info_wrap}>
+                <div className={styles.card_info_title}>Stats</div>
+                <ul>{selectedPokemon.stats.map((stat: StatList) => (
+                  <li className={styles.card_info_stat}
+                    key={stat.stat.name}>
+                    {stat.stat.name} {stat.base_stat}
+                  </li>
+                  ))}
+                </ul>
+              </div>
+              <div className={styles.card_info_wrap}>
+                <div className={styles.card_info_title}>Abilities</div>
+                <ul>{selectedPokemon.abilities.map((ability: AbilityList) => (
+                  <li className={styles.card_info_stat}
+                    key={ability.ability.name}>
+                    {ability.ability.name}
+                  </li>
+                  ))}
+                </ul>
+              </div>
             </div>
 
           </div>
           <ul className={styles.list}>
             {pokemons.map((pokemon) => (
                 <li className={styles.pokemon_item}
-                onClick={() => setSelectedPokemonId(pokemon.id)}
+                  key={pokemon.id}
+                  onClick={() => setSelectedPokemonId(pokemon.id)}
                   tabIndex={0}
                   role='option'
                   aria-selected={setSelectedPokemonId === pokemon.id}
                   onKeyPress={(event) => {
                     if (event.key === 'Enter') setSelectedPokemonId(pokemon.id)
                   }}
-                  key={pokemon.id}
-                  >
+                >
                   <img className={styles.pokemon_item_img} 
                   src={pokemon.sprites.front_default ?? ''} 
                   alt="pokemon img"
